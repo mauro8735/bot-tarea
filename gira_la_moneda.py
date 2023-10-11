9e05cdee11877798c98ca8eba5d6afada68eae00
mauro8735 <145277478+mauro8735@users.noreply.github.com>
2023-10-11 17:23:17 -0300

    gira_la_moneda.py

---
+++ gira_la_moneda.py
@@ -0,0 +1,10 @@
+import random
+
+def g_l_m():
+    s_n = ''
+    x = random.randint(1,2)
+    if x == 1:
+        s_n = 'te salio: si'
+    elif x == 2:
+        s_n = 'te salio: no'
+    return s_n
