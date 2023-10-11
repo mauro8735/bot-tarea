56281162d5d7d9e168517d84145aa066bde01c71
mauro8735 <145277478+mauro8735@users.noreply.github.com>
2023-10-11 17:22:14 -0300

    genpass

---
+++ genpass.py
@@ -0,0 +1,10 @@
+import random
+
+def gen_pass(gen_pass):
+    elements = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    password = ""
+
+    for i in range(15):
+        password += random.choice(elements)
+
+    return password
